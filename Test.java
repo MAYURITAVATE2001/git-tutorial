d9d8326ecc3787981f717c46abff5de8165ba2e6
MAYURITAVATE2001 <tavatemayuri@gmail.com>
2025-02-21 22:37:54 +0530

    version 1.0

---
+++ Test.java
@@ -0,0 +1,8 @@
+//version 1.0
+class demo
+{
+	public static void main(String args[])
+	{
+		System,out,println("Hello my name is mayuri, i love java programming");
+	}
+}
---
+++ demo.java
@@ -0,0 +1,7 @@
+class demo1
+{
+	public static void main(String[] arg)
+	{
+		System.out.println("i am here");
+	}
+}
